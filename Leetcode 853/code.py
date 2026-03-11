7c264dbeda41ba5ce535bb89cfd5247c8dcc0ca0
aashiiguptaa23-dev <aashiiguptaa23@gmail.com>
2026-03-11 14:23:54 +0530

    Create code.py

---
+++ Leetcode 853/code.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target, position, speed):
+        cars = sorted(zip(position, speed), reverse=True)
+
+        fleets = 0
+        last_time = 0
+
+        for p, s in cars:
+            time = (target - p) / s
+            if time > last_time:
+                fleets += 1
+                last_time = time
+
+        return fleets
